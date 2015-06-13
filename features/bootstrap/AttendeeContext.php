4f9f3af3ba46b4795badb435fc4a5cd3ce5ced38
Rocco Palladino <rocky@uchicago.edu>
2015-06-13 17:57:52 -0500

    Append snippets to attendee context

--- features/bootstrap/AttendeeContext.php
+++ features/bootstrap/AttendeeContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 
@@ -9,13 +10,58 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 class AttendeeContext implements Context, SnippetAcceptingContext
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Given I am a meetup member named :arg1
      */
-    public function __construct()
+    public function iAmAMeetupMemberNamed($arg1)
     {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have RSVP'd :arg1 for the event named :arg2
+     */
+    public function iHaveRsvpDForTheEventNamed($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I attend this event and go to check in
+     */
+    public function iAttendThisEventAndGoToCheckIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I select myself from the check-in list for this event
+     */
+    public function iSelectMyselfFromTheCheckInListForThisEvent()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I confirm that I want to check in
+     */
+    public function iConfirmThatIWantToCheckIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a message that I have successfuly checked in
+     */
+    public function iShouldSeeAMessageThatIHaveSuccessfulyCheckedIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see myself as checked-in for this event
+     */
+    public function iShouldSeeMyselfAsCheckedInForThisEvent()
+    {
+        throw new PendingException();
     }
 }
