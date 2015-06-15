d3e1671630982543ecc82e7d098233b5ff1166d0
Rocco Palladino <rocky@uchicago.edu>
2015-06-14 23:19:44 -0500

    Append snippets to web attendee context

--- features/bootstrap/WebAttendeeContext.php
+++ features/bootstrap/WebAttendeeContext.php
@@ -1,9 +1,8 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
@@ -19,5 +18,61 @@ class WebAttendeeContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given I am a meetup member named :arg1
+     */
+    public function iAmAMeetupMemberNamed($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have RSVP'd :arg1 for the event named :arg2
+     */
+    public function iHaveRsvpDForTheEventNamed($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I attend this event and go to check in
+     */
+    public function iAttendThisEventAndGoToCheckIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I select myself from the check-in list for this event
+     */
+    public function iSelectMyselfFromTheCheckInListForThisEvent()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I confirm that I want to check in
+     */
+    public function iConfirmThatIWantToCheckIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a message that I have successfuly checked in
+     */
+    public function iShouldSeeAMessageThatIHaveSuccessfulyCheckedIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see myself as checked-in for this event
+     */
+    public function iShouldSeeMyselfAsCheckedInForThisEvent()
+    {
+        throw new PendingException();
     }
 }
