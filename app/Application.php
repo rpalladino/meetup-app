a37ef1b620a9ee7ce199d90bc9a98657f8116a1b
Rocco Palladino <rocky@uchicago.edu>
2015-06-25 16:04:29 -0500

    Register a DBAL connection in the Silex app, configured to use sqlite for now

--- app/Application.php
+++ app/Application.php
@@ -9,6 +9,13 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         parent::__construct($values);
+        
+        $this->register(new Silex\Provider\DoctrineServiceProvider(), array(
+            'db.options' => array(
+                'driver'   => 'pdo_sqlite',
+                'path'     => __DIR__.'/../database/app.db',
+            ),
+        ));
 
         $this->register(new Silex\Provider\TwigServiceProvider(), array(
           'twig.path' => __DIR__.'/../resources/templates',
