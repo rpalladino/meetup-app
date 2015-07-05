04eb64d6b4a56d6f0f12a8a4ca3f20c340e6b6f7
Rocco Palladino <rocky@uchicago.edu>
2015-07-05 11:02:06 -0500

    Append snippets to event context

--- features/bootstrap/EventContext.php
+++ features/bootstrap/EventContext.php
@@ -1,9 +1,8 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
@@ -11,13 +10,74 @@ use Behat\Gherkin\Node\TableNode;
 class EventContext implements Context, SnippetAcceptingContext
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Given there is an event named :arg1 on :arg2 at :arg3
      */
-    public function __construct()
+    public function thereIsAnEventNamedOnAt($arg1, $arg2, $arg3)
     {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the date is :arg1 at :arg2
+     */
+    public function theDateIsAt($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the member :arg1 RSVP'd :arg2 to :arg3
+     */
+    public function theMemberRsvpDTo($arg1, $arg2, $arg3)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I open the check-in list
+     */
+    public function iOpenTheCheckInList()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see the check-in list for :arg1
+     */
+    public function iShouldSeeTheCheckInListFor($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the check-in list should have :arg1 members
+     */
+    public function theCheckInListShouldHaveMembers($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the check-in list should have :arg1 check-ins
+     */
+    public function theCheckInListShouldHaveCheckIns($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a message that check-ins for :arg1 are not yet allowed
+     */
+    public function iShouldSeeAMessageThatCheckInsForAreNotYetAllowed($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a message that there is no upcoming event
+     */
+    public function iShouldSeeAMessageThatThereIsNoUpcomingEvent()
+    {
+        throw new PendingException();
     }
 }
